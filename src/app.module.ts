c12bf512086d0f48c40e8eb9e8509c2787bc6ae3
balvesD3v <lpbalves7filho@gmail.com>
2023-11-19 14:29:42 -0300

    finish 2.0

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,13 @@ import { TransactionsModule } from './transactions/transactions.module';
 import { PrismaService } from 'prisma/prisma.service';
 import { APP_FILTER } from '@nestjs/core';
 import { HttpExceptionFilter } from './http-exception.filter';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TransactionsModule, AuthModule],
-  controllers: [AuthController],
+  imports: [TransactionsModule],
+  controllers: [],
   providers: [
     PrismaService,
     { provide: APP_FILTER, useClass: HttpExceptionFilter },
-    AuthService,
   ],
 })
 export class AppModule {}
